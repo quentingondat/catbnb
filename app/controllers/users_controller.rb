f5b641f1cbf15bd71653bf13a1b8615d4a6af2f3
Alix Besnier <alix.besnier@gmail.com>
2017-03-14 12:53:32 +0100

    we can edit and delete user profile, nedd some more css

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,9 +5,17 @@ class UsersController < ApplicationController
   end
 
   def edit
+
   end
 
   def update
+    current_user.update(user_params)
+    redirect_to user_path(current_user)
+  end
+
+  def destroy
+    current_user.destroy
+    redirect_to root_path
   end
 
   def dashboard
@@ -17,6 +25,10 @@ class UsersController < ApplicationController
   end
 
   private
+
+  def user_params
+    params.require(:user).permit(:name, :description, :photo, :email, :password)
+  end
 
   def set_user
     @user = User.find(params[:id])
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
 
   get 'dashboard', to: 'users#dashboard'
+  get 'edit-profile', to: 'users#edit'
+  delete 'destroy-profile', to: 'users#destroy'
 
 
   devise_for :users
-  resources :users, only: [:show, :edit, :update]
+  resources :users, only: [:show, :update, :destroy]
+
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :cats
