a4df6e124f7e8a06e74fcd352c1531e172400060
Clement Fernon <clement.fernon@gmail.com>
2017-03-14 17:42:22 +0100

    corrected small error

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,28 @@
+class BookingsController < ApplicationController
+  def show
+  end
+
+  # def new
+  # end
+
+  def create
+    @cat = Cat.find(params[:cat_id])
+    @booking = Booking.new(booking_params)
+    @booking.cat = @cat
+    @booking.user = current_user
+    if booking.save
+      redirect_to booking_path(@booking)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_at, :ends_at)
+  end
+end
---
+++ app/models/booking.rb
@@ -0,0 +1,9 @@
+class Booking < ApplicationRecord
+  belongs_to :cat
+  belongs_to :user
+  validates :cat_id, presence: true
+  validates :user_id, presence: true
+  validates :start_at, presence: true
+  validates :ends_at, presence: true
+  validates :price_per_day, presence: true
+end
