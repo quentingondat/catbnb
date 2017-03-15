8fa75168b8ad266fa81897a1d40a8884272a42c3
Quentin Gondat <quentin.gondat@gmail.com>
2017-03-15 14:29:16 +0100

    commit before launch

--- app/helpers/cats_helper.rb
+++ app/helpers/cats_helper.rb
@@ -6,4 +6,12 @@ module CatsHelper
       image_path "default-cat-picture.jpeg"
     end
   end
+
+  def cat_user_photo_path(photo)
+    if photo
+      cl_image_path photo.path, height: 300, width: 400, crop: :fill
+    else
+      image_path "default-user-picture.jpeg"
+    end
+  end
 end
