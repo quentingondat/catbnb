0f4d670a84f10f0b7c32df4f8ac7a668103ce050
Clement Fernon <clement.fernon@gmail.com>
2017-03-15 15:35:53 +0100

    better booking form

--- app/assets/javascripts/channels/datepicker.js
+++ app/assets/javascripts/channels/datepicker.js
@@ -1,16 +1,10 @@
 $(document).ready(function() {
-  $( ".datepicker" ).datepicker('clearDates', {
+  $( "#booking_period" ).datepicker({
+    inputs: $('.actual_range'),
     format: "dd/mm/yyyy",
-    inputs: $('.actual_range')
     todayHighlight: true,
     toggleActive: true,
     immediateUpdates: true,
     autoclose: true
-  });
-
-  $('.datepicker').on('changeDate', function() {
-      $('#my_hidden_input').val(
-          $('#datepicker').datepicker('getFormattedDate')
-      );
   });
 });
