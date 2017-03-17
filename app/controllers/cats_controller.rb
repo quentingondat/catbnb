1d7e5c0968bf85b38058d8db76da60241a4968b9
Quentin Gondat <quentin.gondat@gmail.com>
2017-03-17 15:11:03 +0100

    add session search values to navbar

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -3,9 +3,13 @@ class CatsController < ApplicationController
   before_action :get_current_user, only: [:create, :update]
 
   def index
+    session[:booking_start_at] = params[:start_at] if params[:start_at].present?
+    session[:booking_end_at] = params[:end_at] if params[:end_at].present?
+    session[:address] = params[:address] if params[:address].present?
     @cats = Cat.near(params[:address] || 'Paris')
-    @cats = @cats.where("start_at >= ?", params[:start_at].to_date) if params[:start_at].present?
-    @cats = @cats.where("end_at <= ?", params[:end_at].to_date) if params[:end_at].present?
+    @cats = @cats.where("start_at <= ?", params[:start_at].to_date) if params[:start_at].present?
+    @cats = @cats.where("end_at >= ?", params[:end_at].to_date) if params[:end_at].present?
+    puts params[:start_at].to_date
     @hash = Gmaps4rails.build_markers(@cats) do |cat, marker|
       marker.lat cat.latitude
       marker.lng cat.longitude
@@ -17,12 +21,12 @@ class CatsController < ApplicationController
     @cat = Cat.find(params[:id])
     @booking = Booking.new
     @cat_coordinates = { lat: @cat.latitude, lng: @cat.longitude }
-    @reviews = @cat.reviews.all
-    if !current_user.nil?
-      @current_user_past_booking = current_user.bookings.find{ |e| (e.cat == @cat) && (e.ends_at < DateTime.now)}
-      @current_user_review_needed = @current_user_past_booking.reviews.find{ |e| e.user_id == current_user.id }.nil?
-      @review = Review.new
-    end
+    # @reviews = @cat.reviews.all
+    # if !current_user.nil?
+    #   @current_user_past_booking = current_user.bookings.find{ |e| (e.cat == @cat) && (e.ends_at < DateTime.now)}
+    #   @current_user_review_needed = @current_user_past_booking.reviews.find{ |e| e.user_id == current_user.id }.nil?
+    #   @review = Review.new
+    # end
   end
 
   def create
